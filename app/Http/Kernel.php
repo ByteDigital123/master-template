b2023f2c984169542bf40af4265aa91a9864e931
Alex <alex@builtbypixel.com>
2020-04-08 08:57:07 +0100

    updates

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \Spatie\Cors\Cors::class
     ];
 
     /**
