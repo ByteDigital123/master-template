ca58bb9a42a1a155f3ed374208e1ff9a93a02be1
Alex <alex@builtbypixel.com>
2020-05-01 11:30:50 +0100

    providers

--- app/Repositories/Course/EloquentCourseRepository.php
+++ app/Repositories/Course/EloquentCourseRepository.php
@@ -17,20 +17,20 @@ class EloquentCourseRepository extends BaseRepository implements CourseInterface
 
     public function create(array $attributes)
     {
-        dd(number_format($attributes['retail_price'] * 100, 0) * 100);
 
-        $provider_price = number_format($attributes['provider_price'] * 100, 0) * 100;
-        $retail_price = number_format($attributes['retail_price'] * 100, 0) * 100;
+
+        $provider_price =intval(floatval($attributes['provider_price'] )*100);
+        $retail_price =intval(floatval($attributes['retail_price'] )*100);
 
         if(isset($attributes['discounted_retail_price'])){
-            $discounted_retail_price = number_format($attributes['discounted_retail_price'] * 100, 0) * 100;
+            $discounted_retail_price =intval(floatval($attributes['discounted_retail_price'] )*100);
         }
 
         $course = $this->model->create([
             'provider_id'           => $attributes['provider']['id'],
             'title'                 => $attributes['title'],
-            'provider_price'        => (int)$provider_price,
-            'retail_price'          => (int)$retail_price,
+            'provider_price'        => $provider_price,
+            'retail_price'          => $retail_price,
             'description'           => $attributes['description'],
             'featured'              => isset($attributes['featured']) ? $attributes['featured'] : false,
             'excerpt'               => $attributes['excerpt'],
@@ -39,7 +39,7 @@ class EloquentCourseRepository extends BaseRepository implements CourseInterface
             'provider_reference_id' => isset($attributes['provider_reference_id']) ? $attributes['provider_reference_id'] : null,
             'modules'               => isset($attributes['modules']) ? $attributes['modules'] : null,
             'skills_learned'        => isset($attributes['skills_learned']) ? $attributes['skills_learned'] : null,
-            'discounted_retail_price'        => isset($attributes['discounted_retail_price']) ? (int)$discounted_retail_price : null
+            'discounted_retail_price'        => isset($attributes['discounted_retail_price']) ? $discounted_retail_price : null
         ]);
 
         $course->categories()->sync(collect($attributes['categories'])->pluck('id'));
@@ -49,13 +49,20 @@ class EloquentCourseRepository extends BaseRepository implements CourseInterface
 
     public function updateById($id, array $attributes)
     {
+        $provider_price =intval(floatval($attributes['provider_price'] )*100);
+        $retail_price =intval(floatval($attributes['retail_price'] )*100);
+
+        if(isset($attributes['discounted_retail_price'])){
+            $discounted_retail_price =intval(floatval($attributes['discounted_retail_price'] )*100);
+        }
+
 
         $course = $this->model->find($id);
         $course->update([
             'provider_id'           => $attributes['provider']['id'],
             'title'                 => $attributes['title'],
-            'provider_price'        => $attributes['provider_price'],
-            'retail_price'          => $attributes['retail_price'],
+            'provider_price'        => $provider_price,
+            'retail_price'          => $retail_price,
             'description'           => $attributes['description'],
             'featured'              => isset($attributes['featured']) ? $attributes['featured'] : false,
             'excerpt'               => $attributes['excerpt'],
@@ -64,7 +71,7 @@ class EloquentCourseRepository extends BaseRepository implements CourseInterface
             'provider_reference_id' => isset($attributes['provider_reference_id']) ? $attributes['provider_reference_id'] : null,
             'modules'               => isset($attributes['modules']) ? $attributes['modules'] : null,
             'skills_learned'        => isset($attributes['skills_learned']) ? $attributes['skills_learned'] : null,
-            'discounted_retail_price'        => isset($attributes['discounted_retail_price']) ? $attributes['discounted_retail_price :'] : null
+            'discounted_retail_price'        => isset($attributes['discounted_retail_price']) ? $discounted_retail_price : null
         ]);
 
         $course->categories()->sync(collect($attributes['categories'])->pluck('id'));
